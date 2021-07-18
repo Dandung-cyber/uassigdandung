7245b545340bd27f85001a69bbf523a11e4df6f5
Dandung-cyber <54461499+Dandung-cyber@users.noreply.github.com>
2021-07-18 14:51:25 +0700

    Add files via upload

---
+++ web kaki gajah2/map/data/Centroids_1.js
@@ -0,0 +1,18 @@
+var json_Centroids_1 = {
+"type": "FeatureCollection",
+"name": "Centroids_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID": 2438.0, "ID_Kec": 3321030.0, "Kecamatan": "Guntur", "Terjangkit": 2.0, "Perawatan": null, "Sembuh": null }, "geometry": { "type": "Point", "coordinates": [ 110.605316288460031, -6.974576958480871 ] } },
+{ "type": "Feature", "properties": { "ID": 2439.0, "ID_Kec": 3321040.0, "Kecamatan": "Sayung", "Terjangkit": 3.0, "Perawatan": null, "Sembuh": null }, "geometry": { "type": "Point", "coordinates": [ 110.518441361911556, -6.92783476712396 ] } },
+{ "type": "Feature", "properties": { "ID": 2440.0, "ID_Kec": 3321050.0, "Kecamatan": "Karang Tengah", "Terjangkit": 7.0, "Perawatan": null, "Sembuh": null }, "geometry": { "type": "Point", "coordinates": [ 110.575031639335165, -6.917112845932214 ] } },
+{ "type": "Feature", "properties": { "ID": 2441.0, "ID_Kec": 3321060.0, "Kecamatan": "Bonang", "Terjangkit": 11.0, "Perawatan": null, "Sembuh": null }, "geometry": { "type": "Point", "coordinates": [ 110.586221773575872, -6.849510819670716 ] } },
+{ "type": "Feature", "properties": { "ID": 2442.0, "ID_Kec": 3321070.0, "Kecamatan": "Demak", "Terjangkit": 2.0, "Perawatan": null, "Sembuh": null }, "geometry": { "type": "Point", "coordinates": [ 110.645889359774728, -6.876662119211947 ] } },
+{ "type": "Feature", "properties": { "ID": 2443.0, "ID_Kec": 3321080.0, "Kecamatan": "Wonosalam", "Terjangkit": 0.0, "Perawatan": null, "Sembuh": null }, "geometry": { "type": "Point", "coordinates": [ 110.673523661665982, -6.911585391477256 ] } },
+{ "type": "Feature", "properties": { "ID": 2444.0, "ID_Kec": 3321090.0, "Kecamatan": "Dempet", "Terjangkit": 6.0, "Perawatan": null, "Sembuh": null }, "geometry": { "type": "Point", "coordinates": [ 110.719918793557312, -6.976146524154954 ] } },
+{ "type": "Feature", "properties": { "ID": 2445.0, "ID_Kec": 3321100.0, "Kecamatan": "Gajah", "Terjangkit": 0.0, "Perawatan": null, "Sembuh": null }, "geometry": { "type": "Point", "coordinates": [ 110.739366122441453, -6.898671457380951 ] } },
+{ "type": "Feature", "properties": { "ID": 2446.0, "ID_Kec": 3321110.0, "Kecamatan": "Karanganyar", "Terjangkit": 2.0, "Perawatan": null, "Sembuh": null }, "geometry": { "type": "Point", "coordinates": [ 110.766861776065028, -6.850740183822976 ] } },
+{ "type": "Feature", "properties": { "ID": 2447.0, "ID_Kec": 3321120.0, "Kecamatan": "Mijen", "Terjangkit": 2.0, "Perawatan": null, "Sembuh": null }, "geometry": { "type": "Point", "coordinates": [ 110.696526612695735, -6.813600560931399 ] } },
+{ "type": "Feature", "properties": { "ID": 2448.0, "ID_Kec": 3321130.0, "Kecamatan": "Wedung", "Terjangkit": 2.0, "Perawatan": null, "Sembuh": null }, "geometry": { "type": "Point", "coordinates": [ 110.6140937435392, -6.771976212484102 ] } }
+]
+}
